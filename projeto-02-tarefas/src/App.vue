773ef7b537eadfa8f311e880ac9898bdcd756ac4
leofideliss <leofipp@hotmail.com>
2022-09-07 12:27:06 -0300

    Components registrados

--- projeto-02-tarefas/src/App.vue
+++ projeto-02-tarefas/src/App.vue
@@ -1,34 +1,41 @@
 <template>
-	<div id="app">
-		<h1>Tarefas</h1>
-	</div>
+  <div id="app">
+    <h1>Tarefas</h1>
+	<Bar></Bar>
+	<InputTask></InputTask>
+	<Tasks></Tasks>
+  </div>
 </template>
 
 <script>
-export default {
+import Bar from "@/components/Bar.vue";
+import InputTask from "@/components/InputTask.vue";
+import Tasks from "@/components/Tasks.vue";
 
-}
+export default {
+  components: { Bar, InputTask, Tasks },
+};
 </script>
 
 <style>
-	body {
-		font-family: 'Lato', sans-serif;
-		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
-		color: #FFF;
-	}
+body {
+  font-family: "Lato", sans-serif;
+  background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+  color: #fff;
+}
 
-	#app {
-		display: flex;
-		flex: 1;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-	}
+#app {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
-	#app h1 {
-		margin-bottom: 5px;
-		font-weight: 300;
-		font-size: 3rem;
-	}
+#app h1 {
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 3rem;
+}
 </style>

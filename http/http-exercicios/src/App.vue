d221882453519d0db2721df7d36203024d541922
leofideliss <leofipp@hotmail.com>
2022-09-19 17:31:54 -0300

    post e get com axios

--- http/http-exercicios/src/App.vue
+++ http/http-exercicios/src/App.vue
@@ -1,26 +1,72 @@
 <template>
-	<div id="app" class="container">
-		<h1>HTTP com Axios</h1>
-	</div>
+  <div id="app" class="container">
+    <h1>HTTP com Axios</h1>
+    <b-card>
+      <b-form-group label="Nome:">
+        <b-form-input
+          type="text"
+          size="lg"
+          v-model="usuario.nome"
+          placeholder="INFORME SEU NOME"
+        >
+        </b-form-input>
+      </b-form-group>
+      <b-form-group label="Email:">
+        <b-form-input
+          type="email"
+          size="lg"
+          v-model="usuario.email"
+          placeholder="INFORME SEU EMAIL"
+        >
+        </b-form-input>
+      </b-form-group>
+      <hr />
+      <b-button @click="salvar" size="lg" variant="primary">Salvar</b-button>
+    </b-card>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      usuario: {
+        nome: "",
+        email: "",
+      },
+    };
+  },
+  methods: {
+    salvar() {
+      this.$http.post("usuarios.json", this.usuario).then(() => {
+        this.usuario.nome = "";
+        this.usuario.email = "";
 
-}
+      });
+    },
+  },
+  //   created() {
+  //     this.$http
+  //       .post("usuarios.json", {
+  //         nome: "Leonardo",
+  //         email: "Leo@gmail.com",
+  //       })
+  //       .then((res) => console.log(res));
+  //   },
+};
 </script>
 
 <style>
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	color: #2c3e50;
-	font-size: 1.5rem;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  font-size: 1.5rem;
 }
 
 #app h1 {
-	text-align: center;
-	margin: 50px;
+  text-align: center;
+  margin: 50px;
 }
 </style>
